236d208c9b346a4907f6a41ecda365cdde90db15
KK <k00kadikis@gmail.com>
2018-09-30 14:01:24 +0300

    Decided on using go instead

---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/lib/pq"
+)
+
+var db *gorm.DB
+
+//TODO: add dotenv
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "kadikis"
+	dbname = "trail_note"
+)
+
+//TODO: split off as seperate models in a model directory
+type (
+	userModel struct {
+		gorm.Model
+		username string
+		password string
+	}
+	trailModel struct {
+		gorm.Model
+		User     userModel `gorm:"foreignkey:_user_id"`
+		_user_id uint
+		name     string
+	}
+	geopointModel struct {
+		gorm.Model
+		Trail     trailModel `gorm:foreignkey:_trail_id`
+		_trail_id uint
+		lat       float64
+		lon       float64
+	}
+	picModel struct {
+		gorm.Model
+		filename string
+	}
+	textModel struct {
+		gorm.Model
+		text string
+	}
+	mediaModel struct {
+		gorm.Model
+		Pic      picModel `gorm:foreignkey:_pic_id`
+		_pic_id  uint
+		Text     textModel `gorm:foreignkey:_text_id`
+		_text_id uint
+	}
+)
+
+func init() {
+	var err error
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
+		host, port, user, dbname)
+
+	db, err = gorm.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.DB().Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	//TODO: don't use gorms automigrate, create migrations instead
+}
+
+func main() {
+	router := gin.Default()
+	router.Use(cors.Default())
+
+	v1 := router.Group("/api/v1/trail")
+	{
+		//TODO: split into more managable form
+		//For frontend
+		v1.GET("/", getTrails)
+		v1.GET("/:id", getSingleTrail)
+		v1.GET("/:id", getGeopoints)
+		//For data import
+		v1.POST("/", createTrail)
+	}
+
+	router.Run() // listen and serve on 0.0.0.0:8080
+}
+
+//TODO: Seperate file for frontend needs
+func getTrails(c *gin.Context) {
+
+}
+
+func getSingleTrail(c *gin.Context) {
+
+}
+
+func getGeopoints(c *gin.Context) {
+
+}
+
+//TODO: Seperate file for data import
+func createTrail(c *gin.Context) {
+
+}
--- src/services/index.js
+++
@@ -1,5 +0,0 @@
-const trailDb = require('./trail-db/trail-db.service.js');
-// eslint-disable-next-line no-unused-vars
-module.exports = function (app) {
-  app.configure(trailDb);
-};
